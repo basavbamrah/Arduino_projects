8a2c9ea1cf81eb0e1ca5e549076de5d2d9c3bc6e
Basav Bamrah <80160941+basavbamrah@users.noreply.github.com>
2021-03-20 10:58:29 +0530

    Update and rename US_Sensor.ino to UltraSonic_Sensor.ino

--- UltraSonic_Sensor.ino
+++ UltraSonic_Sensor.ino
@@ -1,4 +1,9 @@
- #include <LiquidCrystal.h> // includes the LiquidCrystal Library
+/**
+Application of Ultrasonic sensor
+Calculate distance of an object and display it on liquid Crystal Display
+*/
+
+#include <LiquidCrystal.h> // includes the LiquidCrystal Library
 LiquidCrystal lcd(1, 2, 3, 4, 5, 6); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7) 
 const int trigPin = 8;
 const int echoPin = 9;
@@ -23,22 +28,11 @@ digitalWrite(trigPin, LOW);
 duration = pulseIn(echoPin, HIGH);
 distanceCm= duration*0.034/2;
 distanceInch = duration * 0.01330 / 2;
-
-/*String str ="Distance "+ (String)distanceCm +"cm ";
-/*int diff= 16-str.length();
-while(diff>0){
-str=str+" ";
-diff--;
-}*/
-//lcd.print(str.length());
-//lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed*/
-//lcd.print("Distance ");
 lcd.setCursor(0,0);
 lcd.print("Distance "+ (String)distanceCm +"cm");
 lcd.setCursor(0,1);
 lcd.print("Distance "+ (String)distanceInch +"\"");
-//lcd.print(str);// Prints string "Distance" on the LCD
-//lcd.setCursor(0,0);
+
 delay(500);
 
 lcd.clear();
